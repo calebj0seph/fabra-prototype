bdfde4f3d5d2d1f60d4c5e032f4988def8cb928f
Caleb Joseph <calebj0seph@users.noreply.github.com>
2025-02-06 01:53:25 +1100

    Add more rich lighting

--- src/editor/lighting.tsx
+++ src/editor/lighting.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from '@mui/material';
+
 import { Vector3 } from 'three';
 
 /**
@@ -9,23 +11,40 @@ export function Lighting() {
     .normalize()
     .multiplyScalar(20);
 
+  const isDarkTheme = useTheme().palette.mode === 'dark';
+
   return (
     <>
-      <ambientLight intensity={0.5} />
+      {/* Use less ambient light in dark mode */}
+      <ambientLight intensity={isDarkTheme ? 0.6 : 1.5} />
       <directionalLight
         castShadow
-        intensity={2}
+        intensity={3}
         position={directionalLightDirection}
         // Since we're going for quite soft shadows, we don't need a very high resolution for the
         // shadow map
-        shadow-mapSize={[256, 256]}
+        shadow-mapSize={[512, 512]}
         shadow-bias={-0.0005}
         // Blur shadows to make them softer
-        shadow-radius={2}
+        shadow-radius={1.5}
         shadow-blurSamples={16}
         // Limit the depth of the shadow camera to reduce depth buffer artifacts
         shadow-camera-near={1}
         shadow-camera-far={50}
+      />
+      {/* Add some cool fill lighting from above */}
+      <spotLight
+        color="#bdd1ff"
+        intensity={3.5}
+        angle={Math.PI / 6}
+        position={[0, 2, -0.5]}
+      />
+      {/* Add some warm back lighting from behind */}
+      <spotLight
+        color="#ffc993"
+        intensity={2}
+        angle={Math.PI / 6}
+        position={[-2, 0, -1]}
       />
     </>
   );
